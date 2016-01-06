6883774ddc472972782d37553f8342edf3562955
Smart Huang <Smart.Huang@activenetwork.com>
2016-01-06 14:35:43 +0800

    update to use external config file

--- src/Helper/Config.java
+++ src/Helper/Config.java
@@ -9,12 +9,14 @@ package Helper;
  *
  * @author huangsmart
  */
-import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 import org.apache.log4j.Logger;
 
 public class Config {
+
     private static Logger logger = Logger.getLogger(Config.class);
 
     private static Config instance = null;
@@ -22,19 +24,21 @@ public class Config {
 
     protected Config() {
         properties = new Properties();
+
+        InputStream inputStream;
+        properties.setProperty("contactPerson", "联系人");
+        properties.setProperty("contactPhoneNumber", "8008700302");
+        properties.setProperty("purchaser", "采购员");
+        properties.setProperty("user", "sa");
+        properties.setProperty("password", "@ctive123");
+        properties.setProperty("host", "10.109.0.161");
         try {
-            InputStream inputStream;
-            String propFileName = "./config.properties";
-            properties.setProperty("contactPerson", "联系人");
-            properties.setProperty("contactPhoneNumber", "8008700302");
-            properties.setProperty("purchaser", "采购员");
-            properties.setProperty("user", "sa");
-            properties.setProperty("password", "@ctive123");
-            properties.setProperty("host", "10.109.0.161");
-            inputStream = getClass().getResourceAsStream("config.properties");
+            String filePath = System.getProperty("user.dir") + "/config/config.properties";
+            logger.info("loading config from path " + filePath);
+            inputStream = new BufferedInputStream(new FileInputStream(filePath));
             properties.load(inputStream);
-        } catch (IOException e) {
-          logger.error(("Error in opening config file, use defaul values."));
+        } catch (Exception e) {
+            logger.debug("Error in opening config file, use defaul values.", e);
         }
 
     }
